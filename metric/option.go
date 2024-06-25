c2cd008d8d03a7f115534a8e094c5545559d1acc
inoth <inoth.work@gmail.com>
2024-06-25 10:21:54 +0800

    .

--- metric/option.go
+++ metric/option.go
@@ -8,6 +8,12 @@ type option struct {
 	Namespace string `toml:"namespace"`
 }
 
+func WithPort(port string) Option {
+	return func(opt *option) {
+		opt.Port = port
+	}
+}
+
 func WithSubsystem(subsystem string) Option {
 	return func(opt *option) {
 		opt.Subsystem = subsystem
